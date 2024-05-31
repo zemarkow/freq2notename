5fa19ad776ee7e21fa97193b1c32e1781c4c1e4f
Zachary Markow <41491369+zemarkow@users.noreply.github.com>
2024-05-31 02:02:22 -0500

    Add files via upload

---
+++ import_test.py
@@ -0,0 +1,11 @@
+## Program Description
+#
+# Author: Zachary Markow
+# Written 27 March 2024.
+# Last Updated 25 May 2024.
+#
+# Test of freq2notename package importing.
+#
+
+# Import.
+import freq2notename.utils as f2nn_utils
